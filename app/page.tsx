8457c3410fb2043c5f5b5d17de6d4f9f902992dd
Kevinardhana096 <kevinardhana096@gmail.com>
2026-07-23 00:52:11 +0800

    hide GalleryPreview section on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <About />
         <GameConcept />
         <Timeline />
-        <GalleryPreview />
+        {/* <GalleryPreview /> */}
         <FAQ />
         <Contact />
       </main>
