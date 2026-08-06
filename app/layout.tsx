5fb79335ddd51c8fe1cf14174288884006547788
Kevinardhana096 <kevinardhana096@gmail.com>
2026-08-06 11:52:38 +0800

    chore: update browser tab logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   title: "Battle of Champions Season III — SmartSolve: Unlock Your Mind, Ignite The Future",
   description: "Kompetisi akademik untuk siswa SMA/SMK/MA Sederajat se-Sulawesi Selatan. Matematika, logika, pemecahan masalah, dan kerja sama tim dalam atmosfer investigasi yang seru.",
   icons: {
-    icon: "/favicon.ico",
+    icon: "/logo-hitam-boc.jpg",
   },
 };
 
